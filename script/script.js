cd08536292d94199dbc367c224f3179cfa9a3eda
zzupd <zzupd@naver.com>
2021-07-27 15:45:53 +0900

    07.27. 슬라이드쇼&콘텐츠작업중

--- script/script.js
+++ script/script.js
@@ -1,28 +1,60 @@
 $(function(){
 
     // GNB 시작(세로형 1열)
-
-    // $(".mainLi").mouseover(function(){        
+    // $(".mainLi").mouseover(function(){
     //     $(this).children("ul.subMenu").stop().slideDown(400);
-    // });
-
-    // $(".mainLi").mouseout(function(){        
+    // });    
+    // $(".mainLi").mouseout(function(){
     //     $(this).children("ul.subMenu").stop().slideUp(900);
     // });
-
     // GNB 끝(세로형 1열)
 
     // GNB 시작(세로형 2열)
-
-    $(".mainLi").mouseover(function(){        
-        $(this).children("ul.subMenu").stop().slideDown(600);
-    });
-
-    $(".mainLi").mouseout(function(){        
+    $(".mainLi").mouseover(function(){
+        $(this).children("ul.subMenu").stop().fadeIn(800);
+    });    
+    $(".mainLi").mouseout(function(){
         $(this).children("ul.subMenu").stop().fadeOut(200);
     });
-
     // GNB 끝(세로형 2열)
 
+    setInterval(fnSlide, 3000);
+    
+    // slideshow (세로형)
+    // function fnSlide() {
+    //     $("#shuttleFrame").animate(
+    //         {"margin-top": "-350px"},
+    //         1600,
+    //         function() {
+    //             $("#slide a:first-child").insertAfter("#slide a:last-child");
+    //             $(this).css({"margin-top": "0"});
+    //         }
+    //     );
+    // }
+    
+    
+    // slideshow (가로형)
+    // function fnSlide() {
+    //     $("#shuttleFrame").animate(
+    //         {"margin-left": "-780px"},
+    //         1600,
+    //         function() {
+    //             $("#slide a:first-child").insertAfter("#slide a:last-child");
+    //             $(this).css({"margin-left": "0"});
+    //         }
+    //     );
+    // }
+    
+    
+    // slideshow (교차 전환형)
+    function fnSlide() {
+        $("#slide a").eq(0).fadeOut(
+            500,
+            function(){
+                $(this).insertAfter("#slide a:last-child");        
+            }
+            );
+        $("#slide a").eq(1).fadeIn(1000)
+    }
 
 });
